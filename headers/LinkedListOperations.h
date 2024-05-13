82e17eaeaed0f8840104a1c39b5d7e8df2b5a4cf
KortexGit <connormoloney73@gmail.com>
2024-05-13 11:45:00 +0100

    Fix

--- headers/LinkedListOperations.h
+++ headers/LinkedListOperations.h
@@ -1,9 +1,27 @@
 #pragma once
 
+class Node {
+public:
+	int data;
+	Node* next;
+
+	Node(int value);
+};
+
 class LinkedListOperations {
 private:
 	// Variables
+	Node* head;
+
 public:
 	LinkedListOperations(); // Constructor
+
 	// Functions
+	void createLinkedList(int elements[], int len);
+	void updateValueAtIndex(int index, int newValue);
+	void deleteValueAtIndex(int index);
+	void insertValueAtIndex(int index, int value);
+	void sortLinkedList();
+	void printLinkedList() const;
+	void displayLinkedListMenu();
 };
